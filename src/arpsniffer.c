854d578f940407e40c739ed79c5b19016c5b4318
RLhere <2065240670@qq.com>
2019-11-26 16:19:19 +0800

    arpsniffer.c

--- src/arpsniffer.c
+++ src/arpsniffer.c
@@ -37,7 +37,73 @@ int main(int argc, char const *argv[])
 
     if(argc!=2)
     {
-        
+        printf("USAGE:arpsniffer <interface>\n");
+        exit(1);
+    }
+
+    if((descr = pcap_open_live(argv[1],MAXBYTES2CAPTURE,0,512,errbuf))==NULL)
+    {
+        fprintf(stderr,"ERROR:%s\n",errbuf);
+        exit(1);
+    }
+
+    if(pcap_lookupnet(argv[1],&netaddr,&mask,errbuf)==-1)
+    {
+        fprintf(stderr,"ERROR:%s\n",pcap_geterr(descr));
+        exit(1);
+    }
+
+    if(pcap_compile(descr,&fliter,"arp",1,mask)==-1)
+    {
+        fprintf(stderr,"ERROR:%s\n",pcap_geterr(descr));
+        exit(1);
+    }
+
+    if(pcap_setfilter(descr,&fliter)==-1)
+    {
+        fprintf(stderr,"ERROR:%s\n",pcap_geterr(descr));
+        exit(1);
+    }
+
+    while(1)
+    {
+        if((packet = pcap_next(descr,&pkthdr))==NULL)
+        {
+            fprintf(stderr,"ERROR:Error getting the packet.\n",errbuf);
+            exit(1);
+        }
+
+        arpheader = (struct arphdr *)(packet+14);
+
+        printf("\n\nReceived Packet Size:%d bytes\n",pkthdr.len);
+        printf("Hardware type:%s\n",(ntohs(arpheader->htype)==1)?"Ethernet":"Unknown");
+        printf("Protocol type:%s\n",(ntohs(arpheader->ptype)==0x0800)?"IPv4":"Unknown");
+        printf("Operation:%s\n",(ntohs(arpheader->oper)==ARP_REQUEST)?"ARP Request":"ARP Reply");
+
+        if(ntohs(arpheader->htype)==1 && ntohs(arpheader->ptype)==0x0800)
+        {
+            printf("Sender MAC:");
+
+            for(i=0;i<6;i++)
+                printf("%02X:",arpheader->sha[i]);
+
+            printf("\nSender IP:");
+
+            for(i=0;i<4;i++)
+                printf("%d.",arpheader->spa[i]);
+
+            printf("\nTarget MAC:");
+
+            for(i=0;i<6;i++)
+                printf("%02X:",arpheader->tha[i]);
+
+            printf("\nTarget IP:");
+
+            for(i=0;i<4;i++)
+                printf("%d.",arpheader->tpa[i]);
+
+            printf("\n");
+        }
     }
 
     return 0;
