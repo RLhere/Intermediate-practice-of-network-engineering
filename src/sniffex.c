d35d00b080d87fe02848c3ac0ddd14a4e0def987
RLhere <2065240670@qq.com>
2019-11-27 11:35:56 +0800

    --- Auto-Git Commit ---

--- src/sniffex.c
+++ src/sniffex.c
@@ -68,4 +68,107 @@ struct sniff_tcp{
     u_short th_urp;
 };
 
-void got_packet(u_char *args,)
+void got_packet(u_char *args,const struct pcap_pkthdr *header, const u_char *packet);
+
+void print_payload(const char *payload, int len);
+
+void print_hex_ascli_line(const u_char *payload, int len, int offset);
+
+void print_app_usage(void)
+{
+    printf("Usage:%s [interface]\n",APP_NAME);
+    printf("\n");
+    printf("Options:\n");
+    printf("    interface   Listen on <interface> for packets.\n");
+    printf("\n");
+
+    return;
+}
+
+void print_hex_ascii_line(const u_char * payload,int len,int offset)
+{
+    int i;
+    int gap;
+    const u_char *ch;
+
+    printf("%05d    ",offset);
+
+    ch = payload;
+    for(i=0;i<len;i++)
+    {
+        printf("%02x",*ch);
+        ch++;
+
+        if(i==7)
+            printf("");
+    }
+
+    if(len<8)
+        printf("");
+
+    if(len<16)
+    {
+        gap = 16 - len;
+        for(i = 0;i<gap;i++)
+        {
+            printf("    ");
+        }
+    }
+    printf("    ");
+
+    ch = payload;
+    for(i=0;i<len;i++)
+    {
+        if(isprint(*ch))
+            printf("%c",*ch);
+        else
+        {
+            printf(".");
+        }
+
+        ch++;
+    }
+
+    printf("\n");
+
+    return;
+}
+
+void print_payload(const char *payload, int len)
+{
+    int len_rem = len;
+    int line_width = 16;
+    int line_len;
+    int offset = 0;
+    const u_char *ch = payload;
+
+    if(len <= 0)
+        return;
+
+    if(len <= line_width)
+    {
+        printf_hex_ascii_line(ch,len,offset);
+        return;
+    }
+
+    for(;;)
+    {
+        line_len = line_width % len_rem;
+        print_hex_ascii_line(ch,line_len,offset);
+        len_rem = len_rem - line_len;
+        ch = ch + line_len;
+        offset = offset + line_width;
+        if(len_rem<=line_width)
+        {
+            print_hex_ascii_line(ch,len_rem,offset);
+            break;
+        }
+    }
+
+    return;
+}
+
+void got_packet(const u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    static
+}
