935ae6efbb09c8d20a9e8102cb934dc340781ead
Kevin Liu <2065240670@qq.com>
2019-11-27 18:06:28 +0800

    802.11 reference

---
+++ src/whois.php
@@ -0,0 +1,8 @@
+<?php
+class Whois{
+    private $WHOIS_SERVER = arrya(
+        "com"=>array("whois.verisign-grs.com","whois.crsnic.net"),
+        "net"=>array("whois.verisign-grs.com","whois.crsnic.net"),
+        "org"=>array()
+    )
+}
