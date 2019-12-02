8e2c74a58f37a7231888ba9e26936cada19e471c
RLhere <2065240670@qq.com>
2019-12-02 14:05:07 +0800

    --- Auto-Git Commit ---

--- src/Arpsniff2 for IEEE802.11.c
+++ src/Arpsniff2 for IEEE802.11.c
@@ -3,12 +3,15 @@
 #include<signal.h>
 #include<net/if.h>
 #include<pcap.h>
+#include<net/ethernet.h>
 #include<netinet/if_ether.h>
 
 #define ETH_HEADER_SIZE 14
 #define AVS_HEADER_SIZE 64
 #define DATA_80211_FRAME_SIZE 24
 #define LLC_HEADER_SIZE 8
+
+typedef __u_char u_char;
 
 struct snap_header
 {
@@ -24,3 +27,35 @@ char    *device;
 int     verbose = 0;
 pcap_t  *handle;
 int     wired = 0;
+
+void ctrl_c()
+{
+    printf("Exiting\n");
+    pcap_breakloop(handle);
+    pcap_close(handle);
+    exit(0);
+}
+
+void usage(char *name)
+{
+    printf("%s - simple ARP sniffer\n",name);
+    printf("Usage: %s [-i interface] [-l] [-v]\n",name);
+    printf("    -i  interface to sniff on\n");
+    printf("    -l  list available interfaces\n");
+    printf("    -v  print verbose info\n\n");
+    exit(1);
+}
+
+void process_packet(u_char * args, const struct pcap_pkthdr *header, const u_char * packet)
+{
+    struct ether_header *eth_header;
+    struct snap_header  *llc_header;
+    struct ether_arp * arp_packet;
+
+    if(wired)
+    {
+        eth_header = (struct ether_header *)packet;
+        arp_packet = (struct ether_header *)(packet + ETH_HEADER_SIZE);
+        if(ntohs(eth_header->ether_type)!=ETHERTYPE_ARP)return;
+    }
+}
--- src/Connect.c
+++ src/Connect.c
@@ -20,14 +20,29 @@ unsigned int flag = 0;
 int main(int argc, char const *argv[])
 {
     MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
     conn = mysql_init(NULL);
+
     if(!(mysql_real_connect(conn,host,user,pass,dbname,port,unix_socket,flag)))
     {
         fprintf(stderr, "\nError: %s [%d]\n",mysql_error(conn),mysql_errno(conn));
         exit(1);
     }
-    printf("Connect Successful!\n\n");
 
+    mysql_query(conn, "SELECT * FROM users");
+
+    res = mysql_store_result(conn);
+
+    while(row = mysql_fetch_row(res))
+    {
+        printf("%s\t%s\t%s\n", row[0], row[1], row[2]);
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+    
     return EXIT_SUCCESS;
 }
 
